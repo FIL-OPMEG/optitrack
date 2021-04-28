9b6a6dc6245c7ad49cf14a21388afd77659f8eb6
stephaniemellor <74371180+stephaniemellor@users.noreply.github.com>
2021-04-28 16:42:56 +0100

    Update code to get marker position in MRI coords
    Changed the camera marker position to the solved rigid body markers (rather than labeled markers) for stability (more likely to be complete and in reasonable positions in case of interpolation in Motive)
    Added a check that the Motive units are millimeters
    Should now allow for old (.txt) scanner-cast files

--- getMarkerPosInMRIcoords.m
+++ getMarkerPosInMRIcoords.m
@@ -17,40 +17,58 @@ function [ MarkerPosMRI ] = getMarkerPosInMRIcoords( scannerCastTableOfInfo, mar
 %       only set up for data from csv2mat_sm. 
 %   - radAxis: radial axis of the scanner-cast. String s.t. if OPM Z axis
 %       is radial, set to 'RAD', else if Y axis is radial to the head, set
-%       to 'TAN'. Default 'RAD'
+%       to 'TAN'. Default 'TAN'
 %
 % OUTPUT:
 %   - MarkerPosMRI: Native space coordinates of markers. 
 %       Nmarkers x 3 (column = spacial dimension).
 %
 % FUNCTION DEPENDENCIES:
-%    None
+%    icp
 %
 % AUTHOR INFO:
 % Stephanie Mellor, stephanie.mellor.17@ucl.ac.uk
-% 26/1/21
+% 20/4/21
 %
 
 % Set default radial axis
 if nargin < 5
-    radAxis = 'RAD';
+    radAxis = 'TAN';
 end
 
 % Import table of info
 scannerCast = readtable(scannerCastTableOfInfo);
 
 % Get marker positions in native space
-[~, idx, ~] = intersect(scannerCast.Headcast, markerScannerCastSlots, 'stable');
-markersP = [scannerCast.Px(idx), scannerCast.Py(idx), scannerCast.Pz(idx)];
+try
+    [~, idx, ~] = intersect(scannerCast.Headcast, markerScannerCastSlots, 'stable');
+catch
+    [~, idx, ~] = intersect(scannerCast.SlotNo, markerScannerCastSlots, 'stable');
+end
+try
+    markersP = [scannerCast.Px(idx), scannerCast.Py(idx), scannerCast.Pz(idx)];
+catch
+    markersP = [scannerCast.Position_x(idx), scannerCast.Position_y(idx), scannerCast.Position_z(idx)];
+end
 
 % Add the height of the marker
 for i = 1:length(markerHeights)
-    if strcmp(radAxis, 'RAD')
-        markersP(i,:) = markersP(i,:) +...
-            markerHeights(i)*[scannerCast.Ox_RAD(idx(i)), scannerCast.Oy_RAD(idx(i)), scannerCast.Oz_RAD(idx(i))];
-    elseif strcmp(radAxis, 'TAN')
-        markersP(i,:) = markersP(i,:) +...
-            markerHeights(i)*[scannerCast.Ox_TAN(idx(i)), scannerCast.Oy_TAN(idx(i)), scannerCast.Oz_TAN(idx(i))];
+    try
+        if strcmp(radAxis, 'RAD')
+            markersP(i,:) = markersP(i,:) +...
+                markerHeights(i)*[scannerCast.Ox_RAD(idx(i)), scannerCast.Oy_RAD(idx(i)), scannerCast.Oz_RAD(idx(i))];
+        elseif strcmp(radAxis, 'TAN')
+            markersP(i,:) = markersP(i,:) +...
+                markerHeights(i)*[scannerCast.Ox_TAN(idx(i)), scannerCast.Oy_TAN(idx(i)), scannerCast.Oz_TAN(idx(i))];
+        end
+    catch
+        if strcmp(radAxis, 'RAD')
+            markersP(i,:) = markersP(i,:) -...
+                markerHeights(i)*[scannerCast.Z_x(idx(i)), scannerCast.Z_y(idx(i)), scannerCast.Z_z(idx(i))];
+        elseif strcmp(radAxis, 'TAN')
+            markersP(i,:) = markersP(i,:) +...
+                markerHeights(i)*[scannerCast.Y_x(idx(i)), scannerCast.Y_y(idx(i)), scannerCast.Y_z(idx(i))];
+        end
     end
 end
 
@@ -63,9 +81,16 @@ pOptiTrackInit = NaN(3, nMarkers);
 counter = 1;
 while any(isnan(pOptiTrackInit(:)))
     for i = 1:nMarkers
-        pOptiTrackInit(:,i) = MovementData.markers.labeledmarkers(i).data(counter,:)';
+        pOptiTrackInit(:,i) = MovementData.markers.rigidbodymarkers(i).data(counter,1:3)';
     end
     counter = counter + 1;
+end
+
+% Multiply to get in mm
+if strcmp(MovementData.lengthUnits{:}, 'Centimeters')
+    pOptiTrackInit = pOptiTrackInit*10;
+elseif strcmp(MovementData.lengthUnits{:}, 'Meters')
+    pOptiTrackInit = pOptiTrackInit*1000;
 end
 
 % Use ICP to register markersP (from headcast) and pOptiTrackInit (from
@@ -97,7 +122,8 @@ while ER(end) > 10
     iter = iter + 1;
     if iter > 1000
         fprintf('Reached maximum number of iterations\n');
-        error('MRI and optitrack coordinates could not be matched.\nEnsure the correct scanner-cast slots and heights have been used.');
+        break;
+        %error('MRI and optitrack coordinates could not be matched. Ensure the correct scanner-cast slots and heights have been used.');
     end
 end
 clear idxs
