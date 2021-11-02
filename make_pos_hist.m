b6c402fcb3980add2106b94e43cfc615b4f312ac
Robert Seymour <robbyseymour@gmail.com>
2021-11-02 18:01:48 +0000

    small font changes

--- make_pos_hist.m
+++ make_pos_hist.m
@@ -1,10 +1,10 @@
 function make_pos_hist(pos_data,color)
 
 figure;
-h = histfit(pos_data,50);
+h = histogram(pos_data,25);
 
 h(1).FaceColor = color;
-h(2).Color = [.2 .2 .2];
+%h(2).Color = [.2 .2 .2];
 set(gca,'FontSize',18);
 xlabel('Distance (cm)','FontSize',22);
 ylabel('Frequency','FontSize',22);
